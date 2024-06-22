4abae5c0970bf85336ba5a632c296b13e1d81310
Itminan30 <itminan30@gmail.com>
2024-06-22 15:20:45 +0600

    update user done in part3

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -55,7 +55,7 @@ handler._user.post = (requestProperties, callback) => {
                 };
                 // store data in db
                 data.create("users", phone, userObject, (err2) => {
-                    if(!err2) {
+                    if (!err2) {
                         callback(200, {
                             message: "User was created successfully!!!"
                         })
@@ -82,11 +82,11 @@ handler._user.post = (requestProperties, callback) => {
 handler._user.get = (requestProperties, callback) => {
     const phone = typeof (requestProperties.queryStringObject.phone) === "string" && requestProperties.queryStringObject.phone.trim().length === 11 ? requestProperties.queryStringObject.phone : false;
 
-    if(phone) {
+    if (phone) {
         // lookup the user
         data.read("users", phone, (err, u) => {
-            const user = {...parseJSON(u)};
-            if(!err && user) {
+            const user = { ...parseJSON(u) };
+            if (!err && user) {
                 delete user.password;
                 callback(200, user);
             } else {
@@ -104,7 +104,58 @@ handler._user.get = (requestProperties, callback) => {
 
 // put method
 handler._user.put = (requestProperties, callback) => {
+    const firstName = typeof (requestProperties.body.firstName) === "string" && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
 
+    const lastName = typeof (requestProperties.body.lastName) === "string" && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
+
+    const phone = typeof (requestProperties.body.phone) === "string" && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone : false;
+
+    const password = typeof (requestProperties.body.password) === "string" && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
+
+    if(phone) {
+        if(firstName || lastName || password) {
+            // lookup the user
+        data.read("users", phone, (err, userData) => {
+            const user = { ...parseJSON(userData) };
+            if (!err && user) {
+                if(firstName) {
+                    user.firstName = firstName;
+                }
+                if(lastName) {
+                    user.lastName = lastName;
+                }
+                if(password) {
+                    user.password = hash(password);
+                }
+
+                // update the database
+                data.update("users", phone, user, (err2) => {
+                    if(!err2) {
+                        callback(200, {
+                            message: "User updated successfully!!!"
+                        })
+                    } else {
+                        callback(500, {
+                            error: "Error updating the user!!!"
+                        })
+                    }
+                })
+            } else {
+                callback(404, {
+                    error: "Requested User was not found!!!"
+                });
+            }
+        })
+        } else {
+            callback(400, {
+                error: "You haven't given any field for change!!!"
+            });
+        }
+    } else {
+        callback(400, {
+            error: "Phone number incorrect!!!"
+        });
+    }
 }
 
 // delete method
