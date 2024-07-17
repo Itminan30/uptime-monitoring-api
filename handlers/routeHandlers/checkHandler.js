f0c7fe8f1ca06314768fe0f7d96f1d178802a3cf
Itminan30 <itminan30@gmail.com>
2024-07-17 21:49:39 +0600

    create checks done in part 5

--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -8,8 +8,9 @@
 
 // dependencies
 const data = require("../../lib/data");
-const { hash, parseJSON } = require("../../helpers/utilities");
+const { hash, parseJSON, createRandomString } = require("../../helpers/utilities");
 const tokenHandler = require("./tokenHandler");
+const { maxChecks } = require("../../helpers/environments");
 
 // module scaffolding
 const handler = {};
@@ -31,22 +32,114 @@ handler._check = {};
 
 // post method
 handler._check.post = (requestProperties, callback) => {
-    
+    // validate inputs
+    let protocol = typeof (requestProperties.body.protocol) === "string" && ["http", "https"].indexOf(requestProperties.body.protocol) > -1 ? requestProperties.body.protocol : false;
+
+    let url = typeof (requestProperties.body.url) === "string" && requestProperties.body.url.trim().length > 0 ? requestProperties.body.url : false;
+
+    let method = typeof (requestProperties.body.method) === "string" && ["get", "post", "put", "delete"].indexOf(requestProperties.body.method) > -1 ? requestProperties.body.method : false;
+
+    let successCodes = typeof (requestProperties.body.successCodes) === "object" && requestProperties.body.successCodes instanceof Array ? requestProperties.body.successCodes : false;
+
+    let timeoutSeconds = typeof (requestProperties.body.timeoutSeconds) === "number" && requestProperties.body.timeoutSeconds % 1 === 0 && requestProperties.body.timeoutSeconds >= 1 && requestProperties.body.timeoutSeconds <= 5 ? requestProperties.body.timeoutSeconds : false;
+
+    if (protocol && url && method && successCodes && timeoutSeconds) {
+        // Get token form header
+        let token = typeof (requestProperties.headerObject.token) === "string" ? requestProperties.headerObject.token : false;
+
+        // lookup the user phone by reading the token
+        data.read("tokens", token, (err1, tokenData) => {
+            if (!err1 && tokenData) {
+                let userPhone = parseJSON(tokenData).phone;
+                // lookup userdata
+                data.read("users", userPhone, (err2, userData) => {
+                    if (!err2 && userData) {
+                        // Verify token
+                        tokenHandler._token.varify(token, userPhone, (validation) => {
+                            if (validation) {
+                                let userObject = parseJSON(userData);
+                                let userChecks = typeof (userObject.checks) === "object" && userObject.checks instanceof Array ? userObject.checks : [];
+                                
+                                if(userChecks.length < maxChecks) {
+                                    let checkId = createRandomString(20);
+                                    let checkObject = {
+                                        "id": checkId,
+                                        userPhone,
+                                        protocol, 
+                                        url,
+                                        method,
+                                        successCodes,
+                                        timeoutSeconds
+                                    };
+                                    // save the object
+                                    data.create("checks", checkId, checkObject, (err3) => {
+                                        if(!err3) {
+                                            // add cheakId to the user object
+                                            // userObject.checks = userChecks;
+                                            // userObject.checks.push(checkId);
+                                            userChecks.push(checkId);
+                                            userObject.checks = userChecks;
+
+                                            // save the new user data
+                                            data.update("users", userPhone, userObject, (err4) => {
+                                                if(!err4) {
+                                                    // return the data about the new check
+                                                    callback(200, checkObject);
+                                                } else {
+                                                    callback(500, {
+                                                        "error": "There was a problem Updating user after adding checks!!!"
+                                                    });
+                                                }
+                                            })
+                                        } else {
+                                            callback(500, {
+                                                "error": "There was a problem creating checks!!!"
+                                            });
+                                        }
+                                    })
+                                } else {
+                                    callback(401, {
+                                        "error": "User has already reached max check limit!!!"
+                                    });
+                                }
+                            } else {
+                                callback(403, {
+                                    "error": "Token not valid!!!"
+                                });
+                            }
+                        })
+                    } else {
+                        callback(404, {
+                            "error": "User Not found"
+                        });
+                    }
+                })
+            } else {
+                callback(403, {
+                    "error": "Authentication Problem!"
+                });
+            }
+        })
+    } else {
+        callback(400, {
+            "error": "You have a probem in your request"
+        });
+    }
 }
 
 // get method
 handler._check.get = (requestProperties, callback) => {
-    
+
 }
 
 // put method
 handler._check.put = (requestProperties, callback) => {
-    
+
 }
 
 // delete method
 handler._check.delete = (requestProperties, callback) => {
-    
+
 }
 
 module.exports = handler;
